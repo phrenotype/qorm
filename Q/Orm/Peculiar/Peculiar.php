620ded34e8ec0face63b2d1782bfc585f76d5e4c
Paul Robert <paul.contrib@gmail.com>
2022-09-08 22:02:04 +0100

    Reduced Peculiar usleep to 1. Increase timestamp bits to 42.

--- Q/Orm/Peculiar/Peculiar.php
+++ Q/Orm/Peculiar/Peculiar.php
@@ -4,7 +4,7 @@ namespace Q\Orm\Peculiar;
 
 class Peculiar
 {
-    const TIMESTAMP_BITS = 41;
+    const TIMESTAMP_BITS = 42;
     const CUSTOM_BITS = 10;
     const SEQUENCE_BITS = 12;
 
@@ -98,7 +98,7 @@ class Peculiar
         // Save new sequence number
         $this->setSequence(self::$sequence);
         $this->pdo->commit();
-        usleep(2000);
+        usleep(1);
 
 
         self::$lastTimestamp = $currentTimestamp;
@@ -134,6 +134,7 @@ class Peculiar
 
     public static function nextId(int $customId = null)
     {
+        usleep(1);
         if (!self::$instance) {
             $instance = new static($customId);
             self::$instance = $instance;
